6e8efc0b43a3ad5bbc330a102d234c01182b3f61
zzw95 <zhuzhenwei95@gmail.com>
2017-08-20 10:14:06 +0800

    update

--- input_data.py
+++ input_data.py
@@ -52,7 +52,45 @@ def loadRandomData(N=100,D=2,K=3):
         t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
         X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
         Y[ix] = j
-    Y_onehot = np.zeros((K, N*K))
-    Y_onehot[Y.squeeze(),:] = 1
-    return X.T,Y.T, Y_onehot
+    Y_onehot = onehot_encode(Y,K)
+    return X.T,Y.T, Y_onehot.T
 
+def onehot_encode(labels, C):
+    """
+    :param: labels -- vector containing the labels, shape = (number of examples, 1) or (number of examples)
+    :param: C -- number of classes, the depth of the one hot dimension
+    :return: Y -- one hot matrix, shape = (number of examples, C)
+    """
+    Y = np.zeros((labels.shape[0],C))
+    if labels.ndim==1:
+         for i in range(labels.shape[0]):
+            Y[i,labels[i].astype(int)] = 1
+    elif labels.ndim==2:
+         for i in range(labels.shape[0]):
+            Y[i,labels[i,0].astype(int)] = 1
+    else:
+        Y=None
+    return Y
+
+def randomize(datasets, labels):
+    """
+    :param datasets: ndarray, shape(number of examples, None, None) or (number of examples, None)
+    :param labels: ndarray, shape(number of examples, None) or (number of examples,)
+    :return:
+    """
+    permutation = np.random.permutation(labels.shape[0])
+    if datasets.ndim==3:
+        shuffle_dataset=datasets[permutation,:,:]
+    elif datasets.ndim==2:
+        shuffle_dataset=datasets[permutation,:]
+    else:
+        shuffle_dataset=None
+
+    if labels.dim==2:
+        shuffle_labels = labels[permutation,:]
+    elif labels.dim==1:
+        shuffle_labels = labels[permutation]
+    else:
+        shuffle_labels = None
+
+    return shuffle_dataset,shuffle_labels
