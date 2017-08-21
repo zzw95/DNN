12ac95c683d774c9364ea4cdebd8ac22b52e7967
zzw95 <zhuzhenwei95@gmail.com>
2017-08-21 19:57:45 +0800

    gradient checking

---
+++ check.py
@@ -0,0 +1,87 @@
+import input_data
+import nn_model_tf
+import numpy as np
+import nn_model
+import matplotlib.pyplot as plt
+import notMNIST_dataset
+
+def gradient_check(X, Y, layer_dims, layer_types, parameters, epsilon = 1e-5, num_params =8):
+    """
+    check the difference of gradients dW between backward propagation and numerical approxiation
+    :param X: input data of shape (size of input layer, number of examples)
+    :param Y: labels vector of shape (size of output layer, number of examples)
+    :param layer_dims: python array (list) containing the size of each layer including input layer
+    :param layer_types: python list containing the type of each layer: "sigmoid" or "relu" "tanh"
+    :param parameters: python dictionary containing weight matrix and bias vector, {'Wi': , 'bi': }
+    :param epsilon: scalar
+    :param num_params: scalar
+    :return: None
+    """
+    L = len(layer_dims)
+    grad = np.zeros((num_params, L-1))
+    grad_approx = np.zeros((num_params, L-1))
+    for l in range(1,L):
+        print('========This is %dst %s layer========' % (l, layer_types[l-1]))
+        count = 0
+        offset = layer_dims[l-1] // (num_params-1)
+        check_params = range(0, layer_dims[l-1], offset)
+        for i in check_params:
+            print('----This is %dst param----' % i)
+            Wl = parameters['W'+str(l)]
+            print(Wl.shape)
+            print("W%d[0][%d] = %f" % (l, i, Wl[0,i]))
+            Wl_plus = Wl[0][i] + epsilon
+            Wl_minus = Wl[0][i] - epsilon
+            Yhat,caches = nn_model.forword_propagation(X, parameters, layer_dims, layer_types)
+            J = nn_model.comput_cost(Yhat, Y, parameters, layer_types[-1])
+            grads = nn_model.backword_propagation(X, Y, parameters, caches, layer_dims, layer_types)
+            dWl = grads['dW'+str(l)]
+            print(np.sum(dWl>0))
+            grad[count,l-1] = dWl[0,i]
+            print('dW%d[0][%d] = %f' %(l, i,dWl[0,i]))
+            print('J = '+str(J))
+
+            parameters['W'+str(l)][0][i] = Wl_plus
+            print('W%d_plus[0][%d] = %f' % (l, i, parameters['W'+str(l)][0,i]))
+            Yhat,caches = nn_model.forword_propagation(X, parameters, layer_dims, layer_types)
+            J_plus = nn_model.comput_cost(Yhat, Y, parameters, layer_types[-1])
+            print('J_plus = '+str(J_plus))
+
+            parameters['W'+str(l)][0][i] = Wl_minus
+            print('W%d_minus[%d][0] = %f' % (l, i, parameters['W'+str(l)][0,i]))
+            Yhat,caches = nn_model.forword_propagation(X, parameters, layer_dims, layer_types)
+            J_minus = nn_model.comput_cost(Yhat, Y, parameters, layer_types[-1])
+            print('J_minus = '+str(J_minus))
+
+            dWl_0i = (J_plus - J_minus) * 0.5 / epsilon
+            grad_approx[count,l-1] = dWl_0i
+            print('dW%d[0][%d]_approx = %f' % (l, i, dWl_0i))
+
+            count = count + 1
+
+    numerator = np.linalg.norm(grad-grad_approx)
+    denominator = np.linalg.norm(grad)+np.linalg.norm(grad_approx)
+    if denominator==0:
+        difference = 0.0
+    else:
+        difference = numerator/denominator
+    print("********differrence = "+str(difference))
+
+
+def check_sigmoid():
+    X, Y = input_data.loadHandwritingTrainingData()    #X.shape=(1024,387)  Y.shape=(1,387)
+    layer_types= ['relu','tanh','sigmoid']
+    layer_dims=[X.shape[0],100,50, Y.shape[0]]
+    parameters = nn_model.init_params(layer_dims)
+    gradient_check(X, Y, layer_dims, layer_types, parameters)
+
+def check_softmax():
+    X,Y,Y_onehot = input_data.loadRandomData()
+    # X.shape=(2, 300), Y.shape=(1,300), Y_onehot=(3,300)
+    # number of examples = 300, number of classes = 3
+
+    layer_types= ['softmax',]
+    layer_dims=[X.shape[0], Y_onehot.shape[0]]
+    parameters = nn_model.init_params(layer_dims)
+    gradient_check(X, Y_onehot, layer_dims, layer_types, parameters,epsilon = 1e-7, num_params =2)
+
